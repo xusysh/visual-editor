0ed0ebdd1bc5e28093fae07c3f68d75b7fb05a89
xusysh <32706918+xusysh@users.noreply.github.com>
2021-01-08 17:09:13 +0800

    组件编辑重构

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 // import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
+// import VueBus from "vue-bus";
+import store from "@/store";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+// Vue.use(VueBus);
 
 new Vue({
+  store,
   render: (h) => h(App)
 }).$mount("#app");
